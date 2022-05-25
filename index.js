660163fa19534402b4fc359ac97b6e07bbf53a08
glennleo <glennleo@Les-MacBook-Pro.local>
2022-05-25 15:31:06 +0700

    update package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+require("any-observable/register")("rxjs");
 
 // most @actions toolkit packages have async methods
 async function run() {
